04cfdfd193c1736fef6b4342ba2a19c643130937
Moises Colon <mcolon@CAI.LOCAL>
2018-06-29 09:43:34 -0500

    Added navbar to slider

--- js/global.js
+++ js/global.js
@@ -67,3 +67,123 @@ const footerHTML = `
         </div>            
     </div>
 `;
+
+const navbar = `
+    <div class="nav-wrapper">
+        <div class="ctn">
+            <!-- Menu on the left side-->
+            <div class="logo-n-hamburgerIcon">
+                <!-- -->
+                <img src="img/icon-hamburger-menu.svg" id="openMenu" alt="Menu">
+                <a href="#" class="logo-desktop">
+                    <img src="img/logo-block-tune-343w-10kb.png" alt="BlockTune">
+                </a>  
+            </div>
+            <!-- Menu on the right side-->
+             <div class="links">
+                <!-- Mobile Logo -->
+                <div class="hideOnDesktopShowOnMobile mobile-logo">
+                    <a href="#">
+                        <img src="img/logo-block-tune-227w-4a4a4a-5kb.jpg" class="navbarLinksTransition" alt="">
+                    </a>
+                </div>
+                <!-- Regular Links -->
+                <div class="discover-ctn">
+                    <a href="#" class="normalLinks navbarLinksTransition">Discover</a>
+                </div>
+                <div class="login-ctn">
+                    <a href="#" class="normalLinks navbarLinksTransition">Log In</a>
+                </div>
+                <div class="tryitnow-ctn">
+                    <a href="#" class="tryItOut navbarLinksTransition">Try It Now</a>
+                </div>
+                <!-- Social Media -->
+                <div class="hideOnDesktopShowOnMobile socialMedia-ctn">
+                    <a href="#" target="_blank">
+                        <img src="img/logo-facebook-4a4a4a.svg" class="navbarLinksTransition" alt="Facebook: BlockTune">
+                    </a>
+                    <a href="#" target="_blank">
+                        <img src="img/logo-twitter-4a4a4a.svg" class="navbarLinksTransition" alt="Twitter: BlockTune">
+                    </a>
+                    <a href="#" target="_blank">
+                        <img src="img/logo-Instagram-4a4a4a.svg" class="navbarLinksTransition" alt="Instragram: BlockTune">
+                    </a>
+                </div>
+                <!-- Close Menu -->
+                <div class="hideOnDesktopShowOnMobile" id="closeMenu">
+                    <img src="img/icon-close-menu-4a4a4a.svg" class="navbarLinksTransition"  alt="BlockTune: Close Menu">
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+
+// const navbarBT = {
+//     openMenuBtn: document.getElementById('openMenu'),
+//     closeMenuBtn: document.getElementById('closeMenu'),
+//     linksContainer: document.getElementsByClassName('links')[0],
+//     openMobileLinks: document.getElementsByClassName('navbarLinksTransition'),
+
+//     openMenu: () => {
+//         navbarBT.linksContainer.style.marginTop = "0";
+//         let reverseCounter = navbarBT.openMobileLinks.length - 1;
+//         for (let i = 0; i < navbarBT.openMobileLinks.length; i++) {
+//             setTimeout(() => {
+//                 navbarBT.openMobileLinks[reverseCounter].style.opacity = 1;
+//                 --reverseCounter;
+//             }, 300);
+//         }
+//     },
+
+//     closeMenu: () => {
+//         let reverseCounter = navbarBT.openMobileLinks.length - 1;
+//         for (let i = 0; i < navbarBT.openMobileLinks.length; i++) {
+//             setTimeout(() => {
+//                 navbarBT.openMobileLinks[reverseCounter].style.opacity = 0;
+//                 --reverseCounter;
+//             }, 50);
+//         }
+//         navbarBT.linksContainer.style.marginTop = "-500px";
+//     },
+
+//     setMobileMenu: () => {
+//         navbarBT.linksContainer.style.opacity = '1';
+//     },
+
+//     setDesktopMenu: () => {
+
+//     }
+// }
+
+// navbarBT.openMenuBtn.addEventListener('click', () => {
+//     navbarBT.openMenu();
+// });
+// navbarBT.closeMenuBtn.addEventListener('click', () => {
+//     navbarBT.closeMenu();
+// });
+
+// let viewportWidthTracker = window.innerWidth;
+
+// window.addEventListener('resize', () => {
+//     const viewportWidth = window.innerWidth;
+//     const linksContainer = document.getElementsByClassName('links')[0];
+
+//     if (viewportWidth !== viewportWidthTracker && viewportWidth > 992) {
+//         const opacityOnLinks = document.getElementsByClassName('navbarLinksTransition');
+//         linksContainer.style.marginTop = "80px";
+
+//         for (let i = 0; i < opacityOnLinks.length; i++) {
+//             opacityOnLinks[i].style.opacity = '1';
+//         }
+
+//         viewportWidthTracker = viewportWidth;
+//     }
+//     else if (viewportWidth !== viewportWidthTracker && viewportWidth < 992) {
+//         linksContainer.style.opacity = '0';
+//         linksContainer.style.marginTop = '-500px';
+
+//         setTimeout(() => {
+//             linksContainer.style.opacity = '1';
+//         }, 2000);
+//     }
+// });
